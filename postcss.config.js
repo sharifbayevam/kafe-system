8dc5d61aa3510d195ed91287b348226e74f1b126
nazokatxon <sarvarerkaboyeva888@gmail.com>
2026-07-07 15:38:11 +0500

    Postcss config faylidagi konflikt to'liq tozalandi

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 export default {
   plugins: {
-<<<<<<< HEAD
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-};
->>>>>>> c337ca7163694ee9f4d2dfb61a9a8eb50f3c9309
